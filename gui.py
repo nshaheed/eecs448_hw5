f23002871148f66291c169795490bad2c40bf719
RobertWinslow <robertmartinwinslow@gmail.com>
2015-04-22 12:40:06 -0500

    Added dynamically scaling graphical display. The bars are currently hooked up to a random number generator. Screenshot included as exampleGUI.png

--- gui.py
+++ gui.py
@@ -2,14 +2,28 @@
 
 from PyQt4 import QtCore, QtGui
 import sys
+import random
 
 
 class calcHandler:
     def __init__(self):
         self.method = 0
+        self.values = [None,None,None,None,None]
+        self.maximum = None
     def setMethod(self, method):
         self.method = method
-
+    def calc(self):
+        if 0 == self.method:
+            self.values[0] = 2*random.random()
+        elif 1 == self.method:
+            self.values[1] = random.random()
+        elif 2 == self.method:
+            self.values[2] = random.random()
+        elif 3 == self.method:
+            self.values[3] = random.random()
+        elif 4 == self.method:
+            self.values[4] = random.random()
+        self.maximum = max([x for x in self.values if x]+[1])
 
 class UI(QtGui.QWidget):
 
@@ -17,7 +31,7 @@ class UI(QtGui.QWidget):
         super(UI,self).__init__()
         self.initiation()
         self.m = model
-
+        
     def initiation(self):
         
         
@@ -43,7 +57,6 @@ class UI(QtGui.QWidget):
         method_combo.addItem("4-Maha Voting")
         method_combo.addItem("5-Custom Aggregate")
         method_combo.move(20,40)
-
         
 
         #Hook up callbacks. 
@@ -55,11 +68,44 @@ class UI(QtGui.QWidget):
         self.setWindowTitle('Chemical Analysis')    
         self.show()
 
+    def paintEvent(self,e):
+        
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        self.drawGraph(qp)
+        qp.end()
+        
+    def drawGraph(self, qp):
+        if self.m.maximum:
+            color = QtGui.QColor(100, 100, 100)
+            #color.setNamedColor('#d4d4d4')
+            qp.setPen(color)
+            #axis lines
+            qp.setBrush(QtGui.QColor(0, 0, 0))
+            qp.drawRect(50, 350, 300, 0)
+            qp.drawRect(50, 150, 0, 200)
+            qp.drawRect(45, 150, 10, 0)
+            #Label
+            qp.drawText(25,155,'%.1f' % self.m.maximum)
+            #data bars
+            qp.setBrush(QtGui.QColor(150, 0, 0))
+            for i,x in enumerate(self.m.values):
+                if x:
+                   qp.drawRect(60+i*55, 350-200*(x/self.m.maximum), 50, 200*(x/self.m.maximum)) 
+
+        
+
+
     def setMethod(self):
         sender = self.sender()
         self.m.setMethod(sender.currentIndex())
+        
     def clickCalc(self):
-        print(self.sender().text() + str(self.m.method))
+        self.m.calc()
+        self.update()
+        #self.paintEvent()
+        #self.disp_lbl.setText(viewGraph(self.m.values, self.m.max))
+        #self.disp_lbl.setText('hh\nlklk')
 
 
 
