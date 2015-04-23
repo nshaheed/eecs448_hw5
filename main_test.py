df69ff465f4bd2eadbafeea5d80e8706f0b0dec4
Demonslyr <Demonslyr@gmail.com>
2015-04-22 19:48:41 -0500

    Made main_test make a little more sense

--- main_test.py
+++ main_test.py
@@ -2,6 +2,11 @@ import readfile
 import vector_handler
 import classifier
 import math
+####################################
+#Stuff for getting set up
+####################################
+
+
 file1 = open("hw5db1.txt","r")
 file2 = open("hw5db2.txt","r")
 dataIn = readfile.readFile(file1)
@@ -10,9 +15,17 @@ stats = readfile.readStats(file2)
 vectors = vector_handler.vector_holder(dataIn,stats)
 classifier = classifier.classifier()
 
+####################################
+#How to get results from classifier
+####################################
 
 result1 = classifier.directClassify(vectors.vectorArr,vectors.statArr,classifier.method_1)
 result2 = classifier.directClassify(vectors.vectorArr,vectors.statArr,classifier.method_2)
+
+####################################
+#Just testing stuff
+####################################
+
 
 for i in range(0,15):
     if result1[i]:
