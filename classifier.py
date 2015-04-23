2ca150341c851eea477907054985e10e5eb6e6af
Demonslyr <Demonslyr@gmail.com>
2015-04-23 17:34:56 -0500

    Methods 3 and 4 Updated main_test
    Implemented the votingclassify and method 3 and 4 methods
    updated main_test with examples of how to call methods as well as
    included some output stats for testing
    
    Note: the voting methods are set up to return not_active in the event of
    a tie vote

--- classifier.py
+++ classifier.py
@@ -6,36 +6,50 @@ class classifier(object):
     def method_1(self,v1,statA,statStd):
         if len(v1) != len(statA):
             raise ValueError("Input vectors must have the same length: ", v1, statA)
-        #self.dist=0
-        #for i in range (0,15):
-            #self.dist = self.dist + pow((float(v1[i])-float(statA[i])),2)
         return math.sqrt(sum([(float(statA[i]) - float(v1[i]))**2 for i in range(len(v1))]))
-        #return math.sqrt(self.dist)
                 
     # Mahalanobis distance method
     def method_2(self,v1,statA,statStd):
         if len(v1) != len(statA):
             raise ValueError("Input vectors must have the same length: ", v1, statA)
-        #self.dist=0
-        #for i in range (0,15):
-        #    #self.dist = self.dist + pow(((float(v1[i])-float(statA[i]))/float(statStd[i])),2)
-        #return math.sqrt(self.dist)
         return math.sqrt(sum([((float(statA[i]) - float(v1[i]))/float(statStd[i]))**2 for i in range(len(v1))]))
 
     # Euclidian voting method
     def method_3(self,v1,stats):
-        self.method_3_cassifications = []
+        self.Active=0
+        self.N_Active=0
+        for i in range(0,len(v1)):
+            if math.sqrt((float(v1[i])-float(stats[0].vector_data[i]))**2) < math.sqrt((float(v1[i])-float(stats[1].vector_data[i]))**2):#this is biased toward N_Active in the event of a tie
+                self.Active = self.Active+1
+            else:
+                self.N_Active = self.N_Active+1
+        return self.Active > self.N_Active
 
     # Mahalanobis distance method
     def method_4(self,v1,stats):
-        self.method_4_cassifications = []
-
+        self.Active=0
+        self.N_Active=0
+        for i in range(0,len(v1)):
+            if math.sqrt(((float(v1[i])-float(stats[0].vector_data[i]))/float(stats[2].vector_data[i]))**2) < math.sqrt(((float(v1[i])-float(stats[1].vector_data[i]))/float(stats[3].vector_data[i]))**2):#this is biased toward N_Active in the event of a tie
+                self.Active = self.Active+1
+            else:
+                self.N_Active = self.N_Active+1
+        return self.Active > self.N_Active
+    
     #recieves a list of vectors, the list of mean std vectors, and a method to send them too. Gives back a string of
     #  ANANANNANNANANA in order of chemicals. A = active N = Not Active
     def directClassify(self,vector,stats,method):
         self.A_N_Vector = []
         for i in range(0,len(vector)):
             self.A_N_Vector.append(method(vector[i].vector_data, stats[0].vector_data,stats[2].vector_data) < method(vector[i].vector_data,stats[1].vector_data,stats[3].vector_data))
+        return self.A_N_Vector
+
+    #recieves a list of vectors, the list of mean std vectors, and a method to send them too. Gives back a string of
+    # ANANANNANNANANA in order of chemicals. A = active N = Not Active
+    def votingClassify(self,vector,stats,method):
+        self.A_N_Vector = []
+        for i in range(0,len(vector)):
+            self.A_N_Vector.append(method(vector[i].vector_data, stats))
         return self.A_N_Vector
 
     # custom classification method based off of the given four.  Assume True = Active, False = NonActive
--- main_test.py
+++ main_test.py
@@ -1,12 +1,7 @@
+import math
 import readfile
 import vector_handler
 import classifier
-import math
-####################################
-#Stuff for getting set up
-####################################
-
-
 file1 = open("hw5db1.txt","r")
 file2 = open("hw5db2.txt","r")
 dataIn = readfile.readFile(file1)
@@ -15,22 +10,9 @@ stats = readfile.readStats(file2)
 vectors = vector_handler.vector_holder(dataIn,stats)
 classifier = classifier.classifier()
 
-####################################
-#How to get results from classifier
-####################################
 
 result1 = classifier.directClassify(vectors.vectorArr,vectors.statArr,classifier.method_1)
 result2 = classifier.directClassify(vectors.vectorArr,vectors.statArr,classifier.method_2)
-
-####################################
-#Just testing stuff
-####################################
-
-print(len(result1))
-print(result1[1])
-
-print(len(result2))
-print(result1[2])
 
 for i in range(0,15):
     if result1[i]:
